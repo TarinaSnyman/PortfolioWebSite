98fbdcf8d81667496964c34813f51b04b14e8819
TarinaSnyman <tarina.snyman@gmail.com>
2025-06-06 10:35:17 +0200

    Add files
    Add local files I have been working on

---
+++ Home.js
@@ -0,0 +1,15 @@
+
+// smooth scroll to footer instead of jumping down
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent default jump behavior
+        const targetId = this.getAttribute('href').substring(1); // Get the id (e.g., "footer")
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth', // Smooth scrolling
+                block: 'start' // Align the top of the element with the top of the viewport
+            });
+        }
+    });
+});
